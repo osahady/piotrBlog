8ddfa86d622d42fb0d3eb65a49940bbde571575e
osahady <osahady@gmail.com>
2020-03-05 12:37:02 +0300

    array way and json way for translation

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,7 @@
+<?php
+return [
+  'welcome' => 'Welcome to Laravel!',
+  'example_with_value'=> 'Hello :name',
+  //there should be no space before and after the pipe
+  'plural' => '{0} No comments yet|{1} :count comment|[2,*] :count comments'
+];
--- resources/views/home1.blade.php
+++ resources/views/home1.blade.php
@@ -1,7 +1,30 @@
 
 @extends('layout')
 @section('content')
-    <h1>Welcome to laravel course!</h1>
+    <h1>{{ __('messages.welcome') }} </h1>
+
+    <p>
+        {{ __('messages.example_with_value', ['name'=>'جمال']) }}
+    </p>
+    <p>
+        {{ trans_choice('messages.plural', 0) }}
+    </p>
+    <p>
+        {{ trans_choice('messages.plural', 1) }}
+    </p>
+    <p>
+        {{ trans_choice('messages.plural', 2) }}
+    </p>
+    <p>
+        {{ trans_choice('messages.plural', 3) }}
+    </p>
+
+    <p>
+        using json: {{ __('Welcome to Laravel!') }}
+    </p>
+    <p>
+        using json: {{ __('Hello :name', ['name'=>'جمال']) }}
+    </p>
     <p>This is the content of the main page!</p>
 @endsection
    
